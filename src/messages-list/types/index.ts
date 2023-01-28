622aa0414c2e79e93f4fb80ea5a27b8d7720b5e6
Danila Simonov <ek@ek-mail.ru>
2023-01-28 09:48:06 +0100

    lib fixes

--- src/messages-list/types/index.ts
+++ src/messages-list/types/index.ts
@@ -1,4 +1,4 @@
 export * from './AscComparator';
-export * from '../../../__legacy/GenericSortedSource';
 export * from './IBlockchainSourceSubject';
 export * from './IListSource';
+export * from './LowLevelMessagesSource';
